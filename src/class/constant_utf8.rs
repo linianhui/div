1931dfa16970b5ba8af579417756c1c93b017217
lnh <lnhcode@outlook.com>
2019-08-16 23:44:37 +0800

    Add ConstantUtf8

---
+++ src/class/constant_utf8.rs
@@ -0,0 +1,32 @@
+// https://docs.oracle.com/javase/specs/jvms/se12/html/jvms-4.html#jvms-4.4.7
+
+/*
+CONSTANT_Utf8_info {
+    u1 tag;
+    u2 length;
+    u1 bytes[length];
+}
+*/
+
+use crate::class::ConstantTag;
+use crate::class::U8Reader;
+use std::convert::TryInto;
+
+#[derive(Debug)]
+pub struct ConstantUtf8 {
+    pub tag: ConstantTag,
+    pub length: u16,
+    pub bytes: Box<[u8]>,
+}
+
+impl ConstantUtf8 {
+    pub fn new(reader: &mut U8Reader) -> ConstantUtf8 {
+        let length = reader.read_u16();
+        let bytes = reader.read_bytes(length.try_into().unwrap());
+        ConstantUtf8 {
+            tag: ConstantTag::Utf8,
+            length: length,
+            bytes: Box::from(bytes),
+        }
+    }
+}
---
+++ tests/class_test/constant_utf8_test/mod.rs
@@ -0,0 +1,12 @@
+extern crate div;
+use div::class::ConstantTag;
+use div::class::ConstantUtf8;
+use div::class::U8Reader;
+
+#[test]
+fn test_constant_utf8() {
+    let mut u8_reader = U8Reader::new(&[0x00, 0x06, 0x3C, 0x69, 0x6E, 0x69, 0x74, 0x3E]);
+    let constant_utf8 = ConstantUtf8::new(&mut u8_reader);
+    assert_eq!(ConstantTag::Utf8, constant_utf8.tag);
+    assert_eq!(6, constant_utf8.length);
+}
